0fe85b0ceb85511fa5cd3ab675b1643f029104a5
henrikhaus <henrik.utistog.hausberg@outlook.com>
2023-10-23 19:35:23 +0200

    First commit test

---
+++ src/test/java/edu/ntnu/stud/TrainDispatchAppTest.java
@@ -0,0 +1,12 @@
+package edu.ntnu.stud;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainDispatchAppTest {
+
+    @Test
+    void main() {
+    }
+}
