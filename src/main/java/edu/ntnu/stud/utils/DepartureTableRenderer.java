70d9b22f14fdc9c36b375842d73dfd78e0b118b7
henrikhaus <henrik.utistog.hausberg@outlook.com>
2023-12-05 21:44:42 +0100

    Added empty row display support. Minor refactoring.

--- src/main/java/edu/ntnu/stud/utils/DepartureTableRenderer.java
+++ src/main/java/edu/ntnu/stud/utils/DepartureTableRenderer.java
@@ -11,10 +11,10 @@ import java.util.List;
  */
 public class DepartureTableRenderer {
 
-    private static final int SPACING = 2;
-    private static final int MIN_TRAIN_NUMBER_LENGTH = 4;
-    private static final int MIN_TRACK_NUMBER_LENGTH = 2;
-    private static final int MIN_DESTINATION_LENGTH = 10;
+    //Configurable constants
+    private static final int SPACING = 2; // Minimum 1
+    private static final int TABLE_ROWS = 4; // Minimum 1
+    private static final boolean RENDER_EMPTY_ROWS = true;
     private static final String SIDE_FRAME = "|";
 
     /**
@@ -26,11 +26,15 @@ public class DepartureTableRenderer {
      */
     public static String renderDepartureTable(List<TrainDeparture> departures, LocalTime currentTime) {
         StringBuilder output = new StringBuilder();
-        int[] lengths = departureDetailLengths(departures, true);
-        output.append(renderHeader(lengths, currentTime));
-        lengths = departureDetailLengths(departures, false);
-        for (TrainDeparture departure : departures) {
-            output.append(renderTrainDeparture(departure, lengths));
+        int[] headerLengths = departureDetailLengths(departures, true);
+        output.append(renderHeader(headerLengths, currentTime));
+        int[] departureDetailLengths = departureDetailLengths(departures, false);
+        departures.stream()
+                .limit(TABLE_ROWS)
+                .forEach(departure -> output.append(renderTrainDeparture(departure, departureDetailLengths)));
+
+        if (RENDER_EMPTY_ROWS) {
+            output.append(String.valueOf(renderEmptyLine(headerLengths)).repeat(Math.max(0, TABLE_ROWS - departures.size())));
         }
         return output.toString();
     }
@@ -67,6 +71,21 @@ public class DepartureTableRenderer {
                 renderTrainNumber,
                 renderTrack,
                 renderDelay,
+                SIDE_FRAME + "\n");
+    }
+
+    /**
+     * Creates a formatted string for an empty row.
+     *
+     * @param lengths An array of maximum lengths for each column.
+     * @return A formatted empty row string.
+     */
+    private static String renderEmptyLine(int[] lengths) {
+        String departureRowFormat = "%-" + SPACING + "s" + Colors.GRAY + " %-" + SPACING + "s" + Colors.RESET + "  %s";
+
+        return String.format(departureRowFormat,
+                SIDE_FRAME,
+                "-".repeat(lengths[0] + lengths[1] + lengths[2] + lengths[3] + lengths[4] + lengths[5] + SPACING * 6 + 2),
                 SIDE_FRAME + "\n");
     }
 
@@ -108,6 +127,22 @@ public class DepartureTableRenderer {
         int resetCodeLength = (header) ? 0 : Colors.RESET.length(); // Length of resetting ANSI color code
         int colorCodeLength = (header) ? 0 : Colors.GREEN.length(); // Length of one ANSI color code
 
+        // Minimum lengths for each column
+        final int MIN_TIME_LENGTH = 3;
+        final int MIN_LINE_LENGTH = 1;
+        final int MIN_DESTINATION_LENGTH = 10;
+        final int MIN_TRAIN_NUMBER_LENGTH = 4;
+        final int MIN_TRACK_NUMBER_LENGTH = 2;
+        final int MIN_DELAY_LENGTH = 1;
+
+        // Set minimum lengths for each column
+        lengths[0] = MIN_TIME_LENGTH;
+        lengths[1] = MIN_LINE_LENGTH;
+        lengths[2] = MIN_DESTINATION_LENGTH;
+        lengths[3] = MIN_TRAIN_NUMBER_LENGTH;
+        lengths[4] = MIN_TRACK_NUMBER_LENGTH;
+        lengths[5] = MIN_DELAY_LENGTH;
+
         // Find the longest string in each column
         for (TrainDeparture departure : departures) {
             lengths[0] = Math.max(lengths[0], departure.getDepartureTime().toString().length());
@@ -121,9 +156,7 @@ public class DepartureTableRenderer {
         // Adjust lengths for color codes and minimum column widths
         lengths[0] += colorCodeLength;
         lengths[1] += colorCodeLength;
-        lengths[2] = Math.max(lengths[2], MIN_DESTINATION_LENGTH) + resetCodeLength;
-        lengths[3] = Math.max(lengths[3], MIN_TRAIN_NUMBER_LENGTH);
-        lengths[4] = Math.max(lengths[4], MIN_TRACK_NUMBER_LENGTH);
+        lengths[2] += resetCodeLength;
         lengths[5] += colorCodeLength + resetCodeLength + 3; // "min" is 3 chars
 
         return lengths;
